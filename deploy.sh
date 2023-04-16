de2fc5c454ea1317cfffc790d24467d71d650108
bamsumit <bam_sumit@hotmail.com>
2023-04-16 13:48:48 +0545

    Nikola auto commit.
    Nikola version: 8.2.3

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+touch output/bamkumarshrestha.github.io/archive.html
+touch output/bamkumarshrestha.github.io/rss.xml
+touch output/bamkumarshrestha.github.io/galleries/rss.xml
+nikola github_deploy
